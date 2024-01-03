93618d20a379560b7cac307be50b07985f928059
zaidsaid-007 <zaidsaid007@gmail.com>
2024-01-04 00:56:28 +0300

    alphabets new changes

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -3,4 +3,4 @@
 for char in range(ord('a'), ord('z') + 1):
     print("{}".format(chr(char)), end='')
 
-print()  # Add a newline at the end to match the expected output
+ # Add a newline at the end to match the expected output
