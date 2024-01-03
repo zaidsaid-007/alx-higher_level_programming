66e1c4e317d7d1f00236c2d704684982bc914f03
zaidsaid-007 <zaidsaid007@gmail.com>
2024-01-04 01:02:43 +0300

    alpha

---
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+
+for char in range(ord('a'), ord('z') + 1):
+    if chr(char) not in ['q', 'e']:
+        print("{}".format(chr(char)), end='')
+
+# Add a newline at the end to match the expected output
