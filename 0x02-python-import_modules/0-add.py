ec9762b694825c63f118bac7b387ac81cd62bea8
zaidsaid-007 <zaidsaid007@gmail.com>
2024-01-04 21:24:21 +0300

    day 2 of py

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,9 +1,11 @@
+
 #!/usr/bin/python3
 
-from add_0 import add
+if __name__ == "__main__":
+    from add_0 import add
 
-a = 1
-b = 2
+    a = 1
+    b = 2
 
-result = add(a, b)
-print(f"{a} + {b} = {result}")
+    result = add(a, b)
+    print("{} + {} = {}".format(a, b, result))
