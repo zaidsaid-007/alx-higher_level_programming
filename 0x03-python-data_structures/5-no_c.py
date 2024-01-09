b6760d9f49af5d7a224c39f58e112d7d4d6db15b
zaidsaid-007 <zaidsaid007@gmail.com>
2024-01-09 10:55:38 +0300

    No C in my py file

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,11 +1,6 @@
 #!/usr/bin/python3
 
 def no_c(my_string):
-
-    listofchars = list(my_string)
-    for char in listofchars:
-
-        if char == 'c' or char == 'C':
-
-            listofchars.remove(char)
-    return("".join(listofchars))
+    list_of_chars = list(my_string)
+    new_list = [char for char in list_of_chars if char.lower() != 'c']
+    return "".join(new_list)
