82f947325c2fb3bffbfb906b5639ed1fe7cc4af5
zaidsaid-007 <zaidsaid007@gmail.com>
2024-01-23 22:20:55 +0300

    my first square more and more updates

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,11 +1,9 @@
 #!/usr/bin/python3
 # 0-square.py
 # Zaid Mohammed Said <zaidsaid007@gmail.com>
-
 """Define a class Square."""
 
 
 class Square:
     """Represents a square """
-    pass
-    
+    pass 
