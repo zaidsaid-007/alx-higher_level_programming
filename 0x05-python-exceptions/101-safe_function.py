5c0c7c22c4ba891fa48aeb416378f7e266ada6b7
zaidsaid-007 <zaidsaid007@gmail.com>
2024-01-23 21:38:39 +0300

    safe function

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -19,5 +19,5 @@ def safe_function(fct, *args):
         return (result)
     except:
         print("Exception: {}".format(sys.exc_info()[1]), file=sys.stderr)
-        return (None)
+        return None
 
