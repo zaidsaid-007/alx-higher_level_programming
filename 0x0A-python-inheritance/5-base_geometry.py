06a39873f143bfd0384850aab3ef4dcff6138fda
zaidsaid-007 <zaidsaid007@gmail.com>
2024-02-05 22:17:45 +0300

    base_geometry

---
+++ 0x0A-python-inheritance/5-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+# 5-base_geometry.py
+# Zaid Mohammed Said <zaidsaid007@gmail.com>
+"""Defines an empty class BaseGeometry."""
+
+
+class BaseGeometry:
+    """Represent base geometry."""
+    pass
